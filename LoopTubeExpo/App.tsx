190f1e88f2c69645fe1b092d9d62adee60f9e50b
Daehoon Kim <ghsdh3409@gmail.com>
2024-06-24 22:08:41 +0900

    [Part3] CH01_09.유튜브 영상 로드 (Expo)

--- LoopTubeExpo/App.tsx
+++ LoopTubeExpo/App.tsx
@@ -1,20 +1,146 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useCallback, useMemo, useState} from 'react';
+import {
+  Alert,
+  Dimensions,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from '@expo/vector-icons/MaterialIcons';
+import queryString from 'query-string';
+import WebView from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const YT_WIDTH = Dimensions.get('window').width;
+const YT_HEIGHT = YT_WIDTH * (9 / 16);
 
 const styles = StyleSheet.create({
-  container: {
+  safearea: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#242424',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  inputConatainer: {
+    backgroundColor: '#1A1A1A',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 16,
+  },
+  input: {
+    fontSize: 15,
+    color: '#AEAEB2',
+    flex: 1,
+    marginRight: 4,
+  },
+  youtubeContainer: {
+    width: YT_WIDTH,
+    height: YT_HEIGHT,
+    backgroundColor: '#4A4A4A',
   },
 });
+
+const App = () => {
+  const [url, setUrl] = useState('');
+  const [youTubeId, setYouTubeId] = useState('');
+
+  const onPressOpenLink = useCallback(() => {
+    const {
+      query: {v: id},
+    } = queryString.parseUrl(url);
+    console.log('id', id);
+
+    if (typeof id === 'string') {
+      setYouTubeId(id);
+    } else {
+      Alert.alert('잘못된 URL입니다.');
+    }
+  }, [url]);
+
+  const source = useMemo(() => {
+    const html = `
+    <!DOCTYPE html>
+    <html>
+      <body>
+        <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+        <div id="player"></div>
+
+        <script>
+          // 2. This code loads the IFrame Player API code asynchronously.
+          var tag = document.createElement('script');
+
+          tag.src = "https://www.youtube.com/iframe_api";
+          var firstScriptTag = document.getElementsByTagName('script')[0];
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+          // 3. This function creates an <iframe> (and YouTube player)
+          //    after the API code downloads.
+          var player;
+          function onYouTubeIframeAPIReady() {
+            player = new YT.Player('player', {
+              height: '${YT_HEIGHT}',
+              width: '${YT_WIDTH}',
+              videoId: '${youTubeId}',
+              events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+              }
+            });
+          }
+
+          // 4. The API will call this function when the video player is ready.
+          function onPlayerReady(event) {
+            event.target.playVideo();
+          }
+
+          // 5. The API calls this function when the player's state changes.
+          //    The function indicates that when playing a video (state=1),
+          //    the player should play for six seconds and then stop.
+          var done = false;
+          function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING && !done) {
+              setTimeout(stopVideo, 6000);
+              done = true;
+            }
+          }
+          function stopVideo() {
+            player.stopVideo();
+          }
+        </script>
+      </body>
+    </html>
+    `;
+    return {html};
+  }, [youTubeId]);
+
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <View style={styles.inputConatainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="클릭하여 링크를 삽입하세요"
+          placeholderTextColor={'#AEAEB2'}
+          onChangeText={setUrl}
+          value={url}
+          inputMode="url"
+        />
+        <TouchableOpacity
+          onPress={onPressOpenLink}
+          hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}>
+          <Icon name="add-link" size={24} color={'#AEAEB2'} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.youtubeContainer}>
+        {youTubeId.length > 0 && <WebView source={source} />}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
