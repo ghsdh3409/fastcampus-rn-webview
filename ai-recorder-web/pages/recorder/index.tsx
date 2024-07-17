409682c57eb1ecf8f501bcc8b2d84172fb7f442b
Daehoon Kim <ghsdh3409@gmail.com>
2024-07-17 16:27:12 +0900

    [Part4] CH01_03.녹음 화면 구현 (웹)

---
+++ ai-recorder-web/pages/recorder/index.tsx
@@ -0,0 +1,205 @@
+import Header from "@/components/Header";
+import { formatTime } from "@/modules/Util";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const Recorder = () => {
+  const [state, setState] = useState<"recording" | "paused" | null>(null);
+  const [toastVisible, setToastVisible] = useState(false);
+  const [time, setTime] = useState(0);
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
+
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const chunksRef = useRef<Blob[]>([]);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const startTimer = useCallback(() => {
+    timerRef.current = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+  }, []);
+
+  const stopTimer = useCallback(() => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    }
+  }, []);
+
+  const showToast = useCallback(() => {
+    setToastVisible(true);
+  }, []);
+
+  useEffect(() => {
+    if (toastVisible) {
+      const id = setTimeout(() => {
+        setToastVisible(false);
+      }, 1000);
+      return () => {
+        clearTimeout(id);
+      };
+    }
+  }, [toastVisible]);
+
+  const onStartRecord = useCallback(() => {
+    setTime(0);
+    setAudioUrl(null);
+    startTimer();
+    setState("recording");
+  }, [startTimer]);
+
+  const onStopRecord = useCallback(
+    ({ url }: { url: string }) => {
+      setAudioUrl(url);
+      setState(null);
+      stopTimer();
+      showToast();
+    },
+    [stopTimer, showToast]
+  );
+
+  const record = useCallback(() => {
+    window.navigator.mediaDevices
+      .getUserMedia({ audio: true, video: false })
+      .then((stream) => {
+        const mimeType = "audio/webm";
+        const mediaRecorder = new MediaRecorder(stream, { mimeType });
+        mediaRecorderRef.current = mediaRecorder;
+        mediaRecorder.onstart = () => {
+          onStartRecord();
+        };
+        mediaRecorder.ondataavailable = (event) => {
+          chunksRef.current.push(event.data);
+        };
+        mediaRecorder.onstop = () => {
+          const blob = new Blob(chunksRef.current, {
+            type: chunksRef.current[0].type,
+          });
+          chunksRef.current = [];
+          const url = URL.createObjectURL(blob);
+          onStopRecord({ url });
+          stream.getAudioTracks().forEach((track) => track.stop());
+        };
+        mediaRecorder.start();
+      });
+  }, [onStartRecord, onStopRecord]);
+
+  const stop = useCallback(() => {
+    if (mediaRecorderRef.current != null) {
+      mediaRecorderRef.current.stop();
+    }
+  }, []);
+
+  const pause = useCallback(() => {
+    if (mediaRecorderRef.current != null) {
+      mediaRecorderRef.current.pause();
+    }
+  }, []);
+
+  const resume = useCallback(() => {
+    if (mediaRecorderRef.current != null) {
+      mediaRecorderRef.current.resume();
+    }
+  }, []);
+
+  const onPressRecord = useCallback(() => {
+    record();
+  }, [record]);
+
+  const onPressSave = useCallback(() => {
+    stop();
+  }, [stop]);
+
+  const onPressPause = useCallback(() => {
+    if (state === "recording") {
+      pause();
+      stopTimer();
+      setState("paused");
+    } else if (state === "paused") {
+      resume();
+      startTimer();
+      setState("recording");
+    }
+  }, [pause, startTimer, stopTimer, resume, state]);
+
+  return (
+    <div className="h-screen bg-white flex flex-col">
+      <Header title="녹음하기" />
+      <div className="flex flex-1 flex-col items-center pt-[211px]">
+        {state === "recording" ? (
+          <button
+            className="w-[120px] h-[120px] rounded-[80px] bg-[#1A1A1A]"
+            onClick={onPressPause}
+          >
+            <span className="material-icons text-white text-[70px]">mic</span>
+          </button>
+        ) : state === "paused" ? (
+          <button
+            className="w-[120px] h-[120px] rounded-[80px] bg-[#1A1A1A]"
+            onClick={onPressPause}
+          >
+            <span className="material-icons text-white text-[70px]">pause</span>
+          </button>
+        ) : (
+          <button
+            className="w-[120px] h-[120px] rounded-[80px] bg-[#1A1A1A]"
+            onClick={onPressRecord}
+          >
+            <span className="material-icons text-[#09CC7F] text-[70px]">
+              mic
+            </span>
+          </button>
+        )}
+        <p
+          className={`mt-[42px] text-[20px] font-[600] ${
+            state === "recording" || state === "paused"
+              ? "text-[#303030]"
+              : "text-[#AEAEB2]"
+          }`}
+        >
+          {formatTime(time)}
+        </p>
+        {state === "recording" && (
+          <button
+            className="mt-[42px] bg-[#1A1A1A] rounded-[27px] px-[42px] py-[16px] items-center flex"
+            onClick={onPressPause}
+          >
+            <span className="material-icons text-white text-[20px]">pause</span>
+            <span className="ml-[4px] text-[15px] text-white font-[600]">
+              일시 정지
+            </span>
+          </button>
+        )}
+        {(state === "recording" || state === "paused") && (
+          <button
+            className={`${
+              state === "paused" ? "mt-[42px]" : "mt-[16px]"
+            } bg-[#09CC7F] rounded-[27px] px-[42px] py-[16px] items-center flex`}
+            onClick={onPressSave}
+          >
+            <span className="material-icons text-white text-[20px]">check</span>
+            <span className="ml-[4px] text-[15px] text-white font-[600]">
+              저장 하기
+            </span>
+          </button>
+        )}
+        {toastVisible && (
+          <div className="absolute bottom-[21px] flex border-[1.5px] border-[#09CC7F] w-[358px] py-[13px] px-[17px] rounded-[6px] bg-[#F9FEFF]">
+            <span className="material-icons text-[#00DDA8] text-[24px]">
+              check
+            </span>
+            <p className="ml-[7px] text-[15px] font-[600] text-[#4A4A4A]">
+              저장이 완료되었습니다.
+            </p>
+          </div>
+        )}
+        {audioUrl != null && (
+          <audio controls>
+            <source src={audioUrl} />
+          </audio>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Recorder;
